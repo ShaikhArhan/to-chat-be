a88878002dcde3d19048b8b991ae1ce2f33bd72d
Arhan <as8340453@gmail.com>
2025-03-03 11:10:02 +0530

    Updated feature/fixed bug/added new changes

--- app.js
+++ app.js
@@ -60,14 +60,17 @@ const combineRoute = require("./routes/combineRoute");
 app.use("/api", combineRoute);
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => console.log("Connected to MongoDB"))
     .catch((error) => console.log("Error connecting to MongoDB", error));
 
 // Start the server
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);    
+    console.log(`Server running on ${PORT}`);
 });
 // Socket server setup
 const setupSocket = require("./socket/socket");
